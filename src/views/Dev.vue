ea34fb910f37961a0f5aae9f367d11a67cf61ff3
Luke Na <narukeu@outlook.com>
2023-04-19 18:10:25 +0800

    增加开发页面

---
+++ src/views/Dev.vue
@@ -0,0 +1,14 @@
+<template>
+    <div><el-row>
+            <el-col :span="12">
+                <div class="grid-content ep-bg-purple" />11111
+            </el-col>
+            <el-col :span="12">
+                <div class="grid-content ep-bg-purple-light" />22222
+            </el-col>
+        </el-row></div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
