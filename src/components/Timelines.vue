7d010deea45c36b6bb8485ca024f1995be428fff
Luke Na <narukeu@outlook.com>
2023-04-20 21:40:04 +0800

    时间线组件初步完善

---
+++ src/components/Timelines.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-timeline v-for="(item, index) in jsonData" :key="index">
+    <el-timeline-item :timestamp="item.time" placement="top">
+      <el-card>
+        <h4>{{item.title}}</h4>
+        <p>{{ item.paragraph }}</p>
+      </el-card>
+    </el-timeline-item>
+  </el-timeline>
+</template>
+
+<script setup lang="ts">
+import jsonData from '@/data/About/timelines.json'
+</script>
+
+<style></style>
